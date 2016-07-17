83f6ed8db5c4192a5bf9615a3e3452c57e6f3185
Kang Hong Jin <kanghongjin@gmail.com>
2016-07-17 10:31:26 +0800

    remove weird tests

--- src/test/scala/WalkTest.scala
+++ src/test/scala/WalkTest.scala
@@ -9,26 +9,5 @@ import org.scalatest.junit.JUnitRunner
   */
 @RunWith(classOf[JUnitRunner])
 class WalkTest extends FunSuite {
-  test("regex works minimally") {
-    assert(new Walk(Nil).isBugFix("Fixes bug") === true)
-    assert(new Walk(Nil).isBugFix("Closed thing") === true)
-  }
 
-  test("regex can return false") {
-    assert(new Walk(Nil).isBugFix("no bug here") === false)
-  }
-
-  test("regex accounts for boundary") {
-    assert(new Walk(Nil).isBugFix("nofix here") === false)
-  }
-
-  test("regex works even when keywords are in the middle") {
-    assert(new Walk(Nil).isBugFix("we have a fix here") === true)
-  }
-
-  test("regex can handle case") {
-    assert(new Walk(Nil).isBugFix("this ClOSEs that") === true)
-  }
-
-  // TODO test actual walk
 }
